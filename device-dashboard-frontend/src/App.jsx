ded7176b0bd941c9d4315ff97542edd0fb614b50
allancrowley <allancrowleydev@gmail.com>
2024-10-12 21:18:24 +0300

    device-dashboard-frontend initial

---
+++ device-dashboard-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeviceDashboard from './components/DeviceDashboard';
+
+const App = () => {
+    return (
+        <div>
+            <DeviceDashboard />
+        </div>
+    );
+};
+
+export default App;
